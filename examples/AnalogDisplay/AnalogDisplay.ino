2e1072477b77efab290675e8fe323a62971f237c
Kru Praphas Suwanphet <praphas@hotmail.com>
2018-01-26 09:31:44 +0700

    Add files via upload

---
+++ examples/AnalogDisplay/AnalogDisplay.ino
@@ -0,0 +1,13 @@
+#include <send2seg.h>
+byte digitPins[] = {2,3,4,5}; // LSB to MSB
+byte segmentPins[] = {13, 12, 11, 10, 9, 8, 7, 6}; // seg a to g and dp
+void setup()
+{
+  send2seg::set(1, 4, digitPins, segmentPins); // initialize 
+}
+void loop()
+{ 
+  int value=analogRead(A0);
+  send2seg::loadValue(value,2); // load value  and point location
+  delay(100);   
+}
